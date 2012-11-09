eb7bf8601a13f1832a8fbd3a648fa69cceee9685
Jason Tyler <jasonjtyler@gmail.com>
2012-11-09 00:22:15 -0800

    Finished the kernal
    Finsished the vector dot product implementation for the kernal. I
    changed the data type to vector4 so I could do the operation on each
    vector. Refactored the code to make it easier to read.

--- OpenCL.SimpleKernal/main.c
+++ OpenCL.SimpleKernal/main.c
@@ -3,127 +3,181 @@
 #include <conio.h>
 #include "CL/cl.h"
 
+struct vector4
+{
+    float x;
+    float y;
+    float z;
+    float w;
+};
+
 CONSOLE_SCREEN_BUFFER_INFO csbi;
 HANDLE hStdOutput;
 
+cl_program compileProgram(cl_context context, cl_device_id device);
+cl_int executeKernal(cl_program program);
+BOOL initializePlatformDevice(cl_platform_id* platform, cl_device_id* device, cl_context* context, cl_command_queue* queue);
 char* readFile(size_t* fileSize);
 
 void main(void)
 {
-		cl_int error = 0;   // Used to handle error codes
-		cl_uint entryCount = 1;
-		cl_uint platformCount;
-		cl_platform_id platform;
-		cl_context context;
-		cl_command_queue queue;
-		cl_device_id device;
-        cl_program program = NULL;
-        cl_kernel vector_add_kernel = NULL;
-		size_t byteCount;
-        char* build_log;
-        size_t log_size;
-        float* src_a_h;
-        float* src_b_h;
-        float* res_h;
-        float* check;
-        cl_mem src_a_d;
-        cl_mem src_b_d;
-        cl_mem res_d;
-        char* programText;
-        size_t* programTextSize;
-        int mem_size;
-        int i;
-        int size = 3;
-        size_t local_ws = 512;
-        size_t global_ws = 512;
+	cl_platform_id platform = NULL;
+	cl_context context = NULL;
+	cl_command_queue queue = NULL;
+	cl_device_id device = NULL;
+    cl_program program = NULL;
+    struct vector4* sourceVectorsA;
+    struct vector4* sourceVectorsB;
+    float* results;
+    cl_mem bufferA;
+    cl_mem bufferB;
+    cl_mem bufferResults;
+    int i;
+    size_t size = 500000;
+    size_t local_ws = 512;
+    size_t global_ws = 500000;
+    cl_int error = 0;
 
+	//Display console
+    hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (!GetConsoleScreenBufferInfo(hStdOutput, &csbi)) {
+            printf("GetConsoleScreenBufferInfo failed: %d\n", GetLastError());
+            exit(-1);
+    }
 
-		//Display console
-        hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE);
-        if (!GetConsoleScreenBufferInfo(hStdOutput, &csbi))
-        {
-                printf("GetConsoleScreenBufferInfo failed: %d\n", GetLastError());
-                exit(-1);
-        }
+	//Get all necessary components needed to use the OpenCL platform
+    if (!initializePlatformDevice(&platform, &device, &context, &queue)) {
+        exit(-1);
+    }
 
-		// Platform
-		error = clGetPlatformIDs(entryCount, &platform, &platformCount);
-		if (error != CL_SUCCESS) {
-		  printf("Error getting platform id");
-		   exit(error);
-		}
-
-		// Device
-		error = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device, NULL);
-		if (error != CL_SUCCESS) {
-		   printf("Error getting device IDs.");
-		   exit(error);
-		}
-
-		// Context
-		context = clCreateContext(0, 1, &device, NULL, NULL, &error);
-		if (error != CL_SUCCESS) {
-		   printf("Error creating the context.");
-		   exit(error);
-		}
-
-		// Command-queue
-		queue = clCreateCommandQueue(context, device, 0, &error);
-		if (error != CL_SUCCESS) {
-		   printf("Error creating the command queue.");
-		   exit(error);
-        }
-
-        src_a_h = (float*)calloc(size, sizeof(float));
-        src_b_h = (float*)calloc(size, sizeof(float));
-        res_h = (float*)calloc(size, sizeof(float));
+    sourceVectorsA = (struct vector4*)calloc(size, sizeof(struct vector4));
+    sourceVectorsB = (struct vector4*)calloc(size, sizeof(struct vector4));
+    results = (float*)calloc(size, sizeof(float));
         
-        // Initialize both vectors
-        for (i = 0; i < size; i++) {
-            src_a_h[i] = (float)5;
-            src_b_h[i] = (float)5;
-        }
+    // Initialize both vectors
+    for (i = 0; i < size; i++) {
+        sourceVectorsA[i].x = (float)(rand() % 100);
+        sourceVectorsB[i].x = (float)(rand() % 100);
+        sourceVectorsA[i].y = (float)(rand() % 100);
+        sourceVectorsB[i].y = (float)(rand() % 100);
+        sourceVectorsA[i].z = (float)(rand() % 100);
+        sourceVectorsB[i].z = (float)(rand() % 100);
+    }
 
-        mem_size = sizeof(float)*size;
-        // Allocates a buffer of size mem_size and copies mem_size bytes from src_a_h
-        src_a_d = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, mem_size, src_a_h, &error);
-        src_b_d = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, mem_size, src_b_h, &error);
-        res_d = clCreateBuffer(context, CL_MEM_WRITE_ONLY, mem_size, NULL, &error);
+    // Allocates a buffer of size mem_size and copies mem_size bytes from src_a_h
+    bufferA = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(struct vector4)*size, sourceVectorsA, &error);
+    bufferB = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(struct vector4)*size, sourceVectorsB, &error);
+    bufferResults = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float)*size, NULL, &error);
 
-        programTextSize = (size_t*)calloc(1, sizeof(size_t));
-        programText = readFile(&(programTextSize[0]));
+    //Compile the opencl program.
+    program = compileProgram(context, device);
+    if (program == NULL) {
+        printf("Error compiling program.");
+        exit(-1);
+    }
 
-        program = clCreateProgramWithSource(context, 1, &programText, programTextSize, &error);
-        error = clBuildProgram(program, 1, &device, NULL, NULL, NULL);
+    //Execute the simple kernal on the buffered data.
+    error = executeKernal(program, queue, bufferA, bufferB, bufferResults, size);
+    if (error != CL_SUCCESS) {
+        printf("Error executing kernal.");
+        exit(error);
+    }
 
-        // First call to know the proper size
-        clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, 0, NULL, &log_size);
-        build_log = (char*)calloc(log_size+1, sizeof(char));
-        // Second call to get the log
-        clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, log_size, build_log, NULL);
-        build_log[log_size] = '\0';
-        printf("%s\n\n", build_log);
-        free(build_log);
+    //Get the kernal results.
+    clEnqueueReadBuffer(queue, bufferResults, CL_TRUE, 0, sizeof(float)*size, results, 0, NULL, NULL);
 
-        vector_add_kernel = clCreateKernel(program, "vector_dot_product", &error);
+    for (i = 0; i < size; i+=1000)
+    {
+        printf("[%i] %f\n", i, results[i]);
+    }
 
-        error = clSetKernelArg(vector_add_kernel, 0, sizeof(cl_mem), &src_a_d);
-        error |= clSetKernelArg(vector_add_kernel, 1, sizeof(cl_mem), &src_b_d);
-        error |= clSetKernelArg(vector_add_kernel, 2, sizeof(cl_mem), &res_d);
-        error |= clSetKernelArg(vector_add_kernel, 3, sizeof(size_t), &size);
+    printf("\n\n\n\tPress any key to exit...\n");
+    getch();
+}
 
-        error = clEnqueueNDRangeKernel(queue, vector_add_kernel, 1, NULL, &global_ws, &local_ws, 0, NULL, NULL);
+cl_program compileProgram(cl_context context, cl_device_id device)
+{
+    char* programSource = NULL;
+    char* buildLog = NULL;
+    size_t* programSourceSize = NULL;
+    cl_program program = NULL;
+    size_t logSize = 0;
+    cl_int error;
 
-        check = (float*)calloc(size, sizeof(float));
-        clEnqueueReadBuffer(queue, res_d, CL_TRUE, 0, mem_size, check, 0, NULL, NULL);
+    programSourceSize = (size_t*)calloc(1, sizeof(size_t));
+    programSource = readFile(&(programSourceSize[0]));
 
-        for (i = 0; i < 3; i++)
-        {
-            printf("%f", check[i]);
-        }
+    program = clCreateProgramWithSource(context, 1, &programSource, programSourceSize, &error);
+    error = clBuildProgram(program, 1, &device, NULL, NULL, NULL);
 
-        printf("\n\n\n\tPress any key to exit...\n");
-        getch();
+    // First call to know the proper size
+    clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, 0, NULL, &logSize);
+    buildLog = (char*)calloc(logSize+1, sizeof(char));
+
+    // Second call to get the log
+    clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, logSize, buildLog, NULL);
+    buildLog[logSize] = '\0';
+
+    //Output the log
+    printf("%s\n\n", buildLog);
+
+    free(buildLog);
+
+    return program;
+}
+
+cl_int executeKernal(cl_program program, cl_command_queue queue, cl_mem bufferA, cl_mem bufferB, cl_mem bufferResults, size_t size)
+{
+    cl_kernel vector_add_kernel = NULL;
+    cl_int error = 0;
+
+    vector_add_kernel = clCreateKernel(program, "vector_dot_product", &error);
+
+    error = clSetKernelArg(vector_add_kernel, 0, sizeof(cl_mem), &bufferA);
+    error |= clSetKernelArg(vector_add_kernel, 1, sizeof(cl_mem), &bufferB);
+    error |= clSetKernelArg(vector_add_kernel, 2, sizeof(cl_mem), &bufferResults);
+    error |= clSetKernelArg(vector_add_kernel, 3, sizeof(size_t), &size);
+
+    error = clEnqueueNDRangeKernel(queue, vector_add_kernel, 1, NULL, &size, NULL, 0, NULL, NULL);
+
+    return error;
+}
+
+BOOL initializePlatformDevice(cl_platform_id* platform, cl_device_id* device, cl_context* context, cl_command_queue* queue)
+{
+    cl_uint platformCount = 0;
+    cl_int error = 0;
+
+    //Platform
+	error = clGetPlatformIDs(1, platform, &platformCount);
+	if (error != CL_SUCCESS) 
+    {
+		printf("Error getting platform id");
+		return FALSE;
+	}
+
+    //Device
+	error = clGetDeviceIDs(*platform, CL_DEVICE_TYPE_GPU, 1, device, NULL);
+	if (error != CL_SUCCESS) {
+		printf("Error getting device IDs.");
+		return FALSE;
+    }
+
+    // Context
+	*context = clCreateContext(0, 1, device, NULL, NULL, &error);
+	if (error != CL_SUCCESS) {
+		printf("Error creating the context.");
+		return FALSE;
+    }
+
+    // Command-queue
+	*queue = clCreateCommandQueue(*context, *device, 0, &error);
+	if (error != CL_SUCCESS) {
+		printf("Error creating the command queue.");
+		return FALSE;
+    }
+    
+    return TRUE;
 }
 
 char* readFile(size_t* fileSize)
@@ -152,17 +206,3 @@ char* readFile(size_t* fileSize)
     return buffer;
 
 }
-
-/*
-int main(int argc, char **argv){
-	/*
-	cl_platform_id test;
-	cl_uint num;
-	cl_uint ok = 1;
-	clGetPlatformIDs(ok, &test, &num);
-	return 0;
-	
-
-
-
-}*/
--- OpenCL.SimpleKernal/simple.cl
+++ OpenCL.SimpleKernal/simple.cl
@@ -1,9 +1,12 @@
 
-__kernel void vector_dot_product(__global const float* source_a, __global const float* source_b, __global float* result, const int num)
+__kernel void vector_dot_product(__global const float4* source_a, __global const float4* source_b, __global float* result, const int num)
 {
 	const int index = get_global_id(0);
 
-	if (index < num)
-		result[0] += (source_a[index] * source_b[index]);
+	if (index < num) {
+		result[index] = (source_a[index].x * source_b[index].x);
+		result[index] += (source_a[index].y * source_b[index].y);
+		result[index] += (source_a[index].z * source_b[index].z);
+	}
 
 }
